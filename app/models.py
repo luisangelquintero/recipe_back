a6247e5a3345a15dc45964654eaf18341ccf3b09
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-01 18:44:28 -0500

    add requirements, routes and model

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+class Recipe(BaseModel):
+    title: str
+    ingredients: list[str]
+    instructions: str
+    difficulty: str
+    effort: int
+    submitted_by: str
+    timestamp: str
