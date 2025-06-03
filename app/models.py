0a74c9e3dabf34d50ff965354f3ce52c3ead2512
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-02 21:23:24 -0500

    Update data struct

--- app/models.py
+++ app/models.py
@@ -1,13 +1,12 @@
 
+from typing import Optional
 from pydantic import BaseModel
 
 
 class Recipe(BaseModel):
     title: str
-    ingredients: list[str]
+    ingredients: str
     instructions: str
     difficulty: str
-    time: int
-    submitted_by: str
-    timestamp: str
-    id: str
+    minutes: int
+
