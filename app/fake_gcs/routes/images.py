658d64dd3078662c5f8ae3405685c70147866855
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-26 08:08:29 -0500

    Improve upload_image to create unique names

--- app/fake_gcs/routes/images.py
+++ app/fake_gcs/routes/images.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter, File, UploadFile
+from fastapi import APIRouter, File, Form, UploadFile
 from fastapi.responses import FileResponse
+from uuid import uuid4
+import re
 import os
 
 storage_router = APIRouter()
@@ -7,11 +9,21 @@ UPLOAD_DIR = "./fake_gcs/uploads"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
+def sanitize_filename(text: str) -> str:
+    text = text.lower().replace(" ", "_")
+    return re.sub(r"[^a-z0-9\-]", "", text)
+
+
 @storage_router.post("/upload-image")
-async def upload_image(file: UploadFile = File(...)):
-    file_location = f"{UPLOAD_DIR}/{file.filename}"
+async def upload_image(file: UploadFile = File(...),
+                       recipe_title: str = Form(...)):
+    safe_title = sanitize_filename(recipe_title)
+    unique_filename = f"{safe_title}_{uuid4().hex}.jpg"
+    file_location = os.path.join(UPLOAD_DIR, unique_filename)
+
     with open(file_location, "wb") as f:
         f.write(await file.read())
+
     return {"filename:": file.filename, "url": f"/images/{file.filename}"}
 
 
