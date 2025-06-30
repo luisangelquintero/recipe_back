387034d2506a8f417974abb815a44fd1ffa61065
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-29 22:25:19 -0500

    add image metadata to image clean up

--- app/fake_gcs/routes/images.py
+++ app/fake_gcs/routes/images.py
@@ -1,11 +1,15 @@
+import json
+
 from fastapi import APIRouter, File, Form, UploadFile
 from fastapi.responses import FileResponse
 from uuid import uuid4
 import re
 import os
+from datetime import datetime
 
 storage_router = APIRouter()
 UPLOAD_DIR = "./fake_gcs/uploads"
+IMAGE_DB = "./uploads/image_meta.json"
 os.makedirs(UPLOAD_DIR, exist_ok=True)
 
 
@@ -14,9 +18,25 @@ def sanitize_filename(text: str) -> str:
     return re.sub(r"[^a-z0-9\-]", "", text)
 
 
+def save_image_meta(filename: str, recipe_title:str):
+    try:
+        with open(IMAGE_DB, 'r') as f:
+            db = json.load(f)
+    except FileNotFoundError:
+        db = {}
+
+    db[filename] = {
+        "title": recipe_title,
+        "temporary": True,
+        "timestamp": datetime.utcnow().isoformat()
+    }
+
+    with open(IMAGE_DB, 'w') as f:
+        json.dump(db, f, indent=2)
+
+
 @storage_router.post("/upload-image")
-async def upload_image(file: UploadFile = File(...),
-                       recipe_title: str = Form(...)):
+async def upload_image(file: UploadFile = File(...), recipe_title: str = Form(...)):
     safe_title = sanitize_filename(recipe_title)
     unique_filename = f"{safe_title}_{uuid4().hex}.jpg"
     file_location = os.path.join(UPLOAD_DIR, unique_filename)
