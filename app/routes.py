587b4fac4ea13d6cfeb52226154b3bc2d22ae31f
Luis Angel <luis-angelquintero@hotmail.com>
2025-06-29 22:32:47 -0500

    Add update image metadata to DB

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import json
 from fastapi import APIRouter
 from models import Recipe
 from tinydb import TinyDB
@@ -6,11 +7,25 @@ from tinydb import TinyDB
 recipe_router = APIRouter()
 
 db = TinyDB('test_db.json')
+IMAGE_DB = "./uploads/image_meta.json"
 
 
 @recipe_router.post("/recipes")
 def add_recipe(recipe: Recipe):
-    db.insert(recipe.dict())
+    db.insert(recipe.model_dump())
+
+    # mark image as permanent
+    try:
+        with open(IMAGE_DB, 'r') as f:
+            meta = json.load(f)
+        image_name = recipe.imagePath.split("/")[-1]
+        if image_name in meta:
+            meta[image_name]["temporary"] = False
+
+        with open(IMAGE_DB, "w") as f:
+            json.dump(meta, f, indent=2)
+    except Exception as e:
+        print(f"⚠️ Couldn't update image metadata: {e}")
     return {"message": "Recipe added!"}
 
 
