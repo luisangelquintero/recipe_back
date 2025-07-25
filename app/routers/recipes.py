e04487c510aaf305b0411ee3db604b14ad7e5dbf
Luis Angel <luis-angelquintero@hotmail.com>
2025-07-24 22:51:37 -0500

    Fix image path after update

--- app/routers/recipes.py
+++ app/routers/recipes.py
@@ -14,8 +14,6 @@ DATA_PATH = Path(__file__).resolve().parent.parent / "data"
 DATA_PATH.mkdir(parents=True, exist_ok=True)
 db = TinyDB(DATA_PATH / "recipes_dt.json")
 
-#Todo Create recipes endpoint and change this one for recipe
-
 
 def process_image_data(recipe: Recipe):
     try:
@@ -30,13 +28,14 @@ def process_image_data(recipe: Recipe):
                 image_name_with_title = f'{sanitize_filename(recipe.title)}{image_name}'
                 image_metadata[image_name_with_title] = image_metadata.pop(image_name)
                 image_metadata[image_name_with_title]["title"] = recipe.title
+                recipe.imagePath = f"/images/{image_name_with_title}"
                 os.rename(f"{UPLOAD_DIR}/{image_name}", f"{UPLOAD_DIR}/{image_name_with_title}")
 
         with open(IMAGE_DB, "w") as f:
             json.dump(image_metadata, f, indent=2)
     except Exception as e:
         print(f"⚠️ Couldn't update image metadata: {e}")
-        
+
 
 @recipe_router.post("/recipe")
 def add_recipe(recipe: Recipe):
